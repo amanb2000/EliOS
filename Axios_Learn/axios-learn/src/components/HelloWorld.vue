74a0b1b7be445189128be7b4da0a5f6cb09d9c09
Aman Bhargava <amanb2000@gmail.com>
2019-11-23 18:09:04 -0500

    Add Axios template code for consuming API's

---
+++ Axios_Learn/axios-learn/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <p>Weather: {{ info.data.weather[0].main }}</p>
+    <p>Other weather: {{ info.data.weather[0].description }}</p>
+    <p>Temperature: {{ info.data.main.temp }}</p>
+    <p>Clouds: {{ info.data.clouds.all }}</p>
+    <p>Sunset - Sunrise: {{ info.data.sys.sunset - info.data.sys.sunrise }}</p>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: "HelloWorld",
+  data: function() {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('http://api.openweathermap.org/data/2.5/weather?q='+'London'+'&?units=metric&APPID='+'39718834535fb8fff8e625fd17ca5556')
+      .then(response => (this.info = response))
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
