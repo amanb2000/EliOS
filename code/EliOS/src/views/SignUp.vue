4520424d60ad540873ebcb7521825c5f8cca5dc8
Aman Bhargava <amanb2000@gmail.com>
2019-11-23 00:53:59 -0500

    Create new vue project, enable routing.

---
+++ code/EliOS/src/views/SignUp.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class = "sign-up">
+        <p>Welcome! Create your new new account:</p>
+        <input type="text" placeholder="Email"><br>
+        <input type="password" placeholder="Password"><br>
+        <button>Sign Up</button>
+        <span>Or <router-link to="/login">back to login.</router-link></span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'signUp',
+    data() {
+        return {}
+    },
+    methods: {},
+}
+</script>
+
+<style scoped>
+    .signup {
+        margin-top: 40px;
+    }
+    input {
+        margin: 10px 0;
+        width: 20%;
+        padding: 15px;
+    }
+    button {
+        margin-top: 10px;
+        width: 10%;
+        cursor: pointer;
+    }
+    span {
+        display: block;
+        margin-top: 20px;
+        font-size: 11px;
+    }
+</style>
