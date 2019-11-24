e23e2a24f8002eebc637fc768095bac1025973c4
Aman Bhargava <amanb2000@gmail.com>
2019-11-24 05:16:51 -0500

    Small untracked chart changes

---
+++ code/EliOS/src/grapher.js
@@ -0,0 +1,22 @@
+const grapher = {
+    options1: {
+        title: {
+            display: true,
+            text: 'Today\'s Data',
+            fontSize: 20
+        }, 
+        scale: {
+            angleLines: {
+                display: false
+            },
+            gridLines: {
+                // display: false
+            },
+            ticks: {
+                display: false,
+                suggestedMin: 0,
+                suggestedMax: 1
+            },
+        },
+    }
+}
